e2023ecc025b505de1012ec7f2671a9bf2d5e79a
Borislav <rpallot@tpg.com.au>
2012-04-28 04:54:03 +1000

    Begun work on automatically resizing the splash screen based on the size of the window.
    Potentially with a minimum size that will auto centre itself on the page.
    
    This will be a work in progress across the entire project.

---
+++ js/splash.js
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+var browserWidth, browserHeight;
+
+$(document).ready(function(){
+   
+ });
+ 
+ function setBrowserWidth() {
+	browserWidth = $(window).width();
+ }
+ 
+ function setBrowserHeight() {
+	browserHeight = $(window).height();
+ }
